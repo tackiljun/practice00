431d311238f87866d67967dc0039c8ba3066a4e1
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:27:34 +0900

    구구단 9단

---
+++ src/main/java/org/example/test/multiplication9.java
@@ -0,0 +1,18 @@
+package org.example.test;
+
+public class multiplication9 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("==========구구단 9단==========");
+        
+        // 구구단 9단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("9 X " + i + " = " + 9 * i);
+
+        }
+
+    }
+    
+}
