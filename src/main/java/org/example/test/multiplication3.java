defd2a503a017a8ca904a83e91bb4ad26ad94d8b
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:01:12 +0900

    구구단3단

--- src/main/java/org/example/test/multiplication3.java
+++ src/main/java/org/example/test/multiplication3.java
@@ -4,6 +4,8 @@ public class multiplication3 {
 
     public static void main(String[] args) {
 
+        System.out.println("==========구구단 3단==========");
+
         // 구구단 3단.
         for(int i = 1; i < 10; i++) {
 
