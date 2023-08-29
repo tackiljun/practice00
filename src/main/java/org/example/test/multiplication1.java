7f6fc1ffa46f699292043bcdbd1b815aa63ba1f6
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:36:16 +0900

    구구단 1단

---
+++ src/main/java/org/example/test/multiplication1.java
@@ -0,0 +1,13 @@
+package org.example.test;
+
+public class multiplication1 {
+    public static void main(String[] args) {
+        System.out.println("==========구구단 1단==========");
+        
+        // 구구단 1단.
+        for(int i = 1; i < 10; i++) {
+            System.out.println("1 X " + i + " = " + 1 * i);
+        }
+    }
+
+}
