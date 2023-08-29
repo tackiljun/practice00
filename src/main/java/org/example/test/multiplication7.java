da09dbc13ade9e89e90ab1e90abbd34771cd953e
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:23:33 +0900

    구구단7단

---
+++ src/main/java/org/example/test/multiplication7.java
@@ -0,0 +1,17 @@
+package org.example.test;
+
+public class multiplication7 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("==========구구단 7단==========");
+        
+        // 구구단 7단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("7 X " + i + " = " + 7 * i);
+
+        }
+
+    }
+}
