84a82b2c7ab45d4451f38a098c901e9bc847deca
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:12:27 +0900

    구구단 6단

---
+++ src/main/java/org/example/test/multiplication6.java
@@ -0,0 +1,16 @@
+package org.example.test;
+
+public class multiplication6 {
+    
+    public static void main(String[] args) {
+        System.out.println("==========구구단 6단==========");
+        
+        // 구구단 6단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("6 X " + i + " = " + 6 * i);
+
+        }
+    }
+    
+}
