63d379e112409b6e818fb6f9791ae599cc7f147e
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:26:04 +0900

    구구단 8단

---
+++ src/main/java/org/example/test/multiplication8.java
@@ -0,0 +1,16 @@
+package org.example.test;
+
+public class multiplication8 {
+    
+    public static void main(String[] args) {
+        System.out.println("==========구구단 8단==========");
+        
+        // 구구단 8단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("8 X " + i + " = " + 8 * i);
+
+        }
+    }
+    
+}
