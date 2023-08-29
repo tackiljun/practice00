5da4e4417d86712a720c10ad8d9f016631953dea
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:08:54 +0900

    구구단5단

---
+++ src/main/java/org/example/test/multiplication5.java
@@ -0,0 +1,18 @@
+package org.example.test;
+
+public class multiplication5 {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("==========구구단 5단==========");
+        
+        // 구구단 5단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("5 X " + i + " = " + 5 * i);
+
+        }
+
+    }
+    
+}
