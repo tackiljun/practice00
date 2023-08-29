b855a18d46fa3f2d75f9631a9d46fcaa76f847a4
tarkIlJun <tack89826@gmail.com>
2023-08-29 16:00:45 +0900

    구구단 4단

---
+++ src/main/java/org/example/test/multiplication4.java
@@ -0,0 +1,16 @@
+package org.example.test;
+
+public class multiplication4 {
+    
+    public static void main(String[] args) {
+
+        System.out.println("==========구구단 4단==========");
+        
+        // 구구단 4단.
+        for(int i = 1; i < 10; i++) {
+            System.out.println("4 X " + i + " = " + 4 * i);
+        }
+
+    }
+    
+}
