aca4ba543ceba533f8dd8437fd4ed91be83e28a6
tarkIlJun <tack89826@gmail.com>
2023-08-29 15:55:39 +0900

    구구단practice

---
+++ src/main/java/org/example/test/multiplication2.java
@@ -0,0 +1,16 @@
+package org.example.test;
+
+public class multiplication2 {
+
+    public static void main(String[] args) {
+
+        // 구구단 2단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("2 X " + i + " = " + 2 * i);
+
+        }
+
+    }
+
+}
---
+++ src/main/java/org/example/test/multiplication3.java
@@ -0,0 +1,14 @@
+package org.example.test;
+
+public class multiplication3 {
+
+    public static void main(String[] args) {
+
+        // 구구단 3단.
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("3 X " + i + " = " + 3 * i);
+
+        }
+    }
+}
