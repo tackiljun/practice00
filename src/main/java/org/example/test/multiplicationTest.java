ea76015f9222ac2226fa90c0988499e715c425cd
tarkIlJun <tack89826@gmail.com>
2023-09-04 12:32:30 +0900

    multiplication-mix

---
+++ src/main/java/org/example/test/multiplicationTest.java
@@ -0,0 +1,21 @@
+package org.example.test;
+
+public class multiplicationTest {
+    
+    public static void main(String[] args) {
+
+        System.out.println("===============구구단 mix===============");
+        
+        // 구구단 mix.
+        for(int i = 1; i < 10; i++) {
+
+            for(int j = 1; j < 10; j++) {
+
+                System.out.println(("9 X " + i) +  " + " + ("8 X " + j) + " = " + (9 * i) + (8 * j));
+
+            }
+
+        }
+        
+    }
+}
