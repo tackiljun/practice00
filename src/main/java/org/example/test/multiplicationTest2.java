97867fdb802180c0fa69511cfc0e86bee5545fdc
tarkIlJun <tack89826@gmail.com>
2023-09-04 16:43:02 +0900

    구구단-심화2

--- src/main/java/org/example/test/multiplicationTest2.java
+++ src/main/java/org/example/test/multiplicationTest2.java
@@ -4,7 +4,17 @@ public class multiplicationTest2 {
 
     public static void main(String[] args) {
         
-        
+        System.out.println("==========구구단 심화2==========");
+
+        // 구구단 심화2.
+        for(int i = 1; i < 10; i++) {
+
+            int result = (9 * i) + (8 * i);
+
+            System.out.println("(" + "9 X " + i + ")" + " + " + "(" + "8 X " + i + ")" + " = " + result);
+
+        }
+
     }
     
 }
