ef6c17fa91ae6fd150d41724ebc1ff74ac0ac0b4
tarkIlJun <tack89826@gmail.com>
2023-09-05 15:13:42 +0900

    구구단9단 거꾸로

--- src/main/java/org/example/test/multiplicationTest3.java
+++ src/main/java/org/example/test/multiplicationTest3.java
@@ -4,7 +4,13 @@ public class multiplicationTest3 {
 
     public static void main(String[] args) {
         
-        System.out.println("=====구구단 심화3=====");
+        System.out.println("=====구구단 9단 역으로=====");
+
+        for(int i = 10; i > 0; i--) {
+
+            System.out.println("9 X " + i + " = " + 9 * i);
+
+        }
 
     }
     
