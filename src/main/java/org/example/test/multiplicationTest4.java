ed4690c926f123dc6a75a14c5d17f6cf36a42500
tarkIlJun <tack89826@gmail.com>
2023-09-05 15:30:12 +0900

    구구단 8단 거꾸로

--- src/main/java/org/example/test/multiplicationTest4.java
+++ src/main/java/org/example/test/multiplicationTest4.java
@@ -3,6 +3,16 @@ package org.example.test;
 public class multiplicationTest4 {
     
     public static void main(String[] args) {
+            
+        System.out.println("-----=====구구단 8단 거꾸로=====-----");
+
+        for(int i = 9; i > 0; i--) {
+
+            System.out.println("8 X " + i + " = " + 8 * i);
+
+        }
+
+        System.out.println("-----=====구구단 8단 거꾸로=====-----");
         
     }
 
