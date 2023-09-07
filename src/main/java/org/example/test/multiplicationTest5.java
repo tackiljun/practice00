fde74746bb8245d45c8cdb638adfa34d70185910
tarkIlJun <tack89826@gmail.com>
2023-09-07 22:45:31 +0900

    구구단 테스트

--- src/main/java/org/example/test/multiplicationTest5.java
+++ src/main/java/org/example/test/multiplicationTest5.java
@@ -4,7 +4,17 @@ public class multiplicationTest5 {
     
     public static void main(String[] args) {
         
-        
+        // test.
+
+        for(int i = 1; i < 10; i++) {
+
+            System.out.println("2 X " + i + " = " + 2 * i);
+
+            System.out.println("3 X " + i + " = " + 3 * i);
+
+            System.out.println("4 X " + i + " = " + 4 * i);
+
+        }
 
     }
 
