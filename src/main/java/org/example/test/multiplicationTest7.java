edf8c407dc884b6677ee6eccf21a6c17de9f2dbb
tarkIlJun <tack89826@gmail.com>
2023-09-08 12:22:17 +0900

    구구단전체출력테스트

--- src/main/java/org/example/test/multiplicationTest7.java
+++ src/main/java/org/example/test/multiplicationTest7.java
@@ -5,6 +5,15 @@ public class multiplicationTest7 {
     public static void main(String[] args) {
         
         // test.
-        
+        for(int i = 1; i < 10; i++) {
+
+            for(int j = 2; j < 10; j++) {
+
+                System.out.println(j + " X " + i + " = " + j * i);
+
+            }
+
+        }
+
     }
 }
