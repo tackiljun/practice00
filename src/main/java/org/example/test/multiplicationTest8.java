93cbf3fe67c0061c6819a263224c1cee2f066163
tarkIlJun <tack89826@gmail.com>
2023-09-08 14:37:35 +0900

    구구단 2단부터 9단까지 차례로

---
+++ src/main/java/org/example/test/multiplicationTest8.java
@@ -0,0 +1,23 @@
+package org.example.test;
+
+public class multiplicationTest8 {
+    
+    public static void main(String[] args) {
+
+        System.out.println("=====구구단 2단부터 9단까지 차례로=====");
+        
+        // 정리.
+        for(int j = 2; j < 10; j++) {
+
+            for(int i = 1; i < 10; i++) {
+
+                System.out.println(j + " X " + i + " = " + j * i);
+                // 완성.
+
+            }
+
+        }
+
+    }
+
+}
