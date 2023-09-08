13f5b40c42a2782c5245db2820ab60a9c036c1ed
tarkIlJun <tack89826@gmail.com>
2023-09-08 11:07:29 +0900

    구구단전체출력 테스트

--- src/main/java/org/example/test/multiplicationTest6.java
+++ src/main/java/org/example/test/multiplicationTest6.java
@@ -8,21 +8,28 @@ public class multiplicationTest6 {
 
         for(int i = 1; i < 10; i++) {
 
-            System.out.println("2 X " + i + " = " + 2 * i);
+            // System.out.println("2 X " + i + " = " + 2 * i);
 
-            System.out.println("3 X " + i + " = " + 3 * i);
+            // System.out.println("3 X " + i + " = " + 3 * i);
 
-            System.out.println("4 X " + i + " = " + 4 * i);
+            // System.out.println("4 X " + i + " = " + 4 * i);
 
-            System.out.println("5 X " + i + " = " + 5 * i);
+            // System.out.println("5 X " + i + " = " + 5 * i);
 
-            System.out.println("6 X " + i + " = " + 6 * i);
+            // System.out.println("6 X " + i + " = " + 6 * i);
 
-            System.out.println("7 X " + i + " = " + 7 * i);
+            // System.out.println("7 X " + i + " = " + 7 * i);
 
-            System.out.println("8 X " + i + " = " + 8 * i);
+            // System.out.println("8 X " + i + " = " + 8 * i);
 
-            System.out.println("9 X " + i + " = " + 9 * i);
+            // System.out.println("9 X " + i + " = " + 9 * i);
+
+            // 2부터 9를 j값으로 바꾸면?
+            for(int j = 2; j < 10; j++) {
+
+                
+
+            }
 
         }
 
